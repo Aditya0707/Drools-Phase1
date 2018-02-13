5350de92699561978497cc5f10b596859f0edfb9
adityaNuthalapati <aditya.nuthalapati@ndus.edu>
2018-02-13 09:49:11 -0500

    SQL Server connection

--- src/main/java/TestClaim/ClaimInfo.java
+++ src/main/java/TestClaim/ClaimInfo.java
@@ -13,7 +13,7 @@ public class ClaimInfo implements Serializable {
 	private int rel_emp_id;
 	private int primacy;
 	private int proc_medicare_covered;
-	private String pvd_Zipcode;
+	private int pvd_Zipcode;
 	private Date line_date;
 	private Date current_date;
 	private Date Previous_Date;
@@ -22,213 +22,306 @@ public class ClaimInfo implements Serializable {
 	private String[] event_names;
 	private Date[] event_dates;
 	private List<EventInfo> eventinfo;
-		private int emp_size;
-	private boolean recoverable;
+		private int emp_size_Aged;
+		private int emp_size_disabled;
+	private static boolean recoverable;
 	private boolean primary_member;
+	private Date[] event_start_dates = new Date[100];
+	private Date[] event_end_dates = new Date[100];
+	
 
-	public ClaimInfo(String _clm_id, int _line_num, int _primacy, int _proc_medicare_covered, String _pvd_Zipcode,
-			String _part_type, String[] _event_name, String _mbs_type, int _emp_size, String _line_date,
-			String[] _event_dates, Date _current_date, Date _Previous_Date, boolean _primary_member) throws Exception {
-		clm_id = _clm_id;
+	
+	public ClaimInfo(String _clm_id, int _line_num, int _primacy, int _proc_medicare_covered, int _pvd_Zipcode,
+			String _part_type, String[] _event_name, String _mbs_type, int _emp_size_aged,int _emp_size_disabled, Date _line_date,
+			String[] _event_start_date,String[] _event_end_date, Date _current_date, Date _Previous_Date, boolean _primary_member) throws Exception {
+		
+		clm_id = _clm_id; 	
 		line_num = _line_num;
 		primacy = _primacy;
 		proc_medicare_covered = _proc_medicare_covered;
 		pvd_Zipcode = _pvd_Zipcode;
-
 		part_type = _part_type;
+		
 		event_names = _event_name;
 		mbs_type = _mbs_type;
-		emp_size = _emp_size;
-		line_date = new SimpleDateFormat("dd/MM/yyyy").parse(_line_date);
-		event_dates = new Date[_event_dates.length];
-		for (int i = 0; i < _event_dates.length; i++) {
-			event_dates[i] = new SimpleDateFormat("dd/MM/yyyy").parse(_event_dates[i]);
+		emp_size_Aged = _emp_size_aged;
+		emp_size_disabled = _emp_size_disabled;
+		line_date = _line_date;
 		
-		}
-			 
-		eventinfo = new ArrayList<EventInfo>();
-				
-		for (int j = 0; j < event_names.length; j++) {
-				eventinfo.add(new EventInfo(event_names[j], event_dates[2 * j], event_dates[(2 * j) + 1]));
-				
+		for (int i = 0; i < _event_start_date.length; i++) {
+			
+			if(_event_start_date[i] != null){
+			event_start_dates[i] = new SimpleDateFormat("MM-dd-yyyy").parse(_event_start_date[i]);
+			}else{
+				event_start_dates[i]=_current_date;
 			}
+		}
+		for (int i = 0; i < _event_end_date.length; i++) {
+			if(_event_end_date[i] != null){
+			event_end_dates[i] = new SimpleDateFormat("MM-dd-yyyy").parse(_event_end_date[i]);
+			}else{
+				event_end_dates[i]=_current_date;
+			}
+		}
 		
 		current_date = _current_date;
 		Previous_Date = _Previous_Date;
 		primary_member = _primary_member;
-
+		
+		eventinfo = new ArrayList<EventInfo>();
+		
+		for (int j = 0; j < event_names.length; j++) {
+			
+				eventinfo.add(new EventInfo(event_names[j], event_start_dates[j], event_end_dates[j]));
+				
+			}
+	
 	}
+	
 
 	public  boolean completeEventInfo(List<EventInfo> s, String a, Date linedate){
 		for(int i=0;i<s.size();i++){
-			if((s.get(i).getEvent_name().equals(a)) && linedate.before(s.get(i).getEvent_end_date()) && linedate.after(s.get(i).getEvent_start_Date())){
+			if((s.get(i).getEvent_name().equals(a)) 
+					&& linedate.before(s.get(i).getEvent_end_date()) 
+					&& linedate.after(s.get(i).getEvent_start_Date()))
+			{
 				return true;
 			}
 		}
 		return false;
 		
 	}
-	
-	public List<EventInfo> getEventinfo() {
-		return eventinfo;
-	}
 
-	public void setEventinfo(List<EventInfo> eventinfo) {
-		this.eventinfo = eventinfo;
-	}
-	
 
 	public String getClm_id() {
 		return clm_id;
 	}
 
-	public int getLine_num() {
-		return line_num;
-	}
-
-	public String[] getEvent_names() {
-		return event_names;
-	}
-
-	public void setEvent_names(String[] event_names) {
-		this.event_names = event_names;
-	}
-
-	public Date[] getEvent_dates() {
-		return event_dates;
-	}
-
-	public void setEvent_dates(Date[] event_dates) {
-		this.event_dates = event_dates;
-	}
-
-	public Date getPrevious_Date() {
-		return Previous_Date;
-	}
-
-	public String getPvd_id() {
-		return pvd_id;
-	}
-
-	public String getMbs_id() {
-		return mbs_id;
-	}
-
-	public int getRel_psn_id() {
-		return rel_psn_id;
-	}
-
-	public int getRel_emp_id() {
-		return rel_emp_id;
-	}
-
-	public String getPart_type() {
-		return part_type;
-	}
-
-	public int getEmp_size() {
-		return emp_size;
-	}
-
-	public int getPrimacy() {
-		return primacy;
-	}
 
 	public void setClm_id(String clm_id) {
 		this.clm_id = clm_id;
 	}
 
+
+	public int getLine_num() {
+		return line_num;
+	}
+
+
 	public void setLine_num(int line_num) {
 		this.line_num = line_num;
 	}
+
+
+	public String getPvd_id() {
+		return pvd_id;
+	}
+
 
 	public void setPvd_id(String pvd_id) {
 		this.pvd_id = pvd_id;
 	}
 
+
+	public String getMbs_id() {
+		return mbs_id;
+	}
+
+
 	public void setMbs_id(String mbs_id) {
 		this.mbs_id = mbs_id;
 	}
+
+
+	public int getRel_psn_id() {
+		return rel_psn_id;
+	}
+
 
 	public void setRel_psn_id(int rel_psn_id) {
 		this.rel_psn_id = rel_psn_id;
 	}
 
+
+	public int getRel_emp_id() {
+		return rel_emp_id;
+	}
+
+
 	public void setRel_emp_id(int rel_emp_id) {
 		this.rel_emp_id = rel_emp_id;
 	}
+
+
+	public int getPrimacy() {
+		return primacy;
+	}
+
 
 	public void setPrimacy(int primacy) {
 		this.primacy = primacy;
 	}
 
-	public void setProc_medicare_covered(int proc_medicare_covered) {
-		this.proc_medicare_covered = proc_medicare_covered;
-	}
-
-	public void setPvd_Zipcode(String pvd_Zipcode) {
-		this.pvd_Zipcode = pvd_Zipcode;
-	}
-
-	public void setLine_date(Date line_date) {
-		this.line_date = line_date;
-	}
-
-	public void setCurrent_date(Date current_date) {
-		this.current_date = current_date;
-	}
-
-	public void setPrevious_Date(Date previous_Date) {
-		Previous_Date = previous_Date;
-	}
-
-	public void setPart_type(String part_type) {
-		this.part_type = part_type;
-	}
-
-	public void setMbs_type(String mbs_type) {
-		this.mbs_type = mbs_type;
-	}
-
-	public void setEmp_size(int emp_size) {
-		this.emp_size = emp_size;
-	}
 
 	public int getProc_medicare_covered() {
 		return proc_medicare_covered;
 	}
 
-	public String getPvd_Zipcode() {
+
+	public void setProc_medicare_covered(int proc_medicare_covered) {
+		this.proc_medicare_covered = proc_medicare_covered;
+	}
+
+
+	public int getPvd_Zipcode() {
 		return pvd_Zipcode;
 	}
+
+
+	public void setPvd_Zipcode(int pvd_Zipcode) {
+		this.pvd_Zipcode = pvd_Zipcode;
+	}
+
 
 	public Date getLine_date() {
 		return line_date;
 	}
 
-	public String getMbs_type() {
-		return mbs_type;
+
+	public void setLine_date(Date line_date) {
+		this.line_date = line_date;
 	}
+
 
 	public Date getCurrent_date() {
 		return current_date;
 	}
 
-	public boolean isRecoverable() {
+
+	public void setCurrent_date(Date current_date) {
+		this.current_date = current_date;
+	}
+
+
+	public Date getPrevious_Date() {
+		return Previous_Date;
+	}
+
+
+	public void setPrevious_Date(Date previous_Date) {
+		Previous_Date = previous_Date;
+	}
+
+
+	public String getPart_type() {
+		return part_type;
+	}
+
+
+	public void setPart_type(String part_type) {
+		this.part_type = part_type;
+	}
+
+
+	public String getMbs_type() {
+		return mbs_type;
+	}
+
+
+	public void setMbs_type(String mbs_type) {
+		this.mbs_type = mbs_type;
+	}
+
+
+	public String[] getEvent_names() {
+		return event_names;
+	}
+
+
+	public void setEvent_names(String[] event_names) {
+		this.event_names = event_names;
+	}
+
+
+	public Date[] getEvent_dates() {
+		return event_dates;
+	}
+
+
+	public void setEvent_dates(Date[] event_dates) {
+		this.event_dates = event_dates;
+	}
+
+
+	public List<EventInfo> getEventinfo() {
+		return eventinfo;
+	}
+
+
+	public void setEventinfo(List<EventInfo> eventinfo) {
+		this.eventinfo = eventinfo;
+	}
+
+
+	public int getEmp_size_Aged() {
+		return emp_size_Aged;
+	}
+
+
+	public void setEmp_size_Aged(int emp_size_Aged) {
+		this.emp_size_Aged = emp_size_Aged;
+	}
+
+
+	public int getEmp_size_disabled() {
+		return emp_size_disabled;
+	}
+
+
+	public void setEmp_size_disabled(int emp_size_disabled) {
+		this.emp_size_disabled = emp_size_disabled;
+	}
+
+
+	public static boolean isRecoverable() {
 		return recoverable;
 	}
 
+
 	public void setRecoverable(boolean recoverable) {
-		this.recoverable = recoverable;
+		ClaimInfo.recoverable = recoverable;
 	}
+
 
 	public boolean isPrimary_member() {
 		return primary_member;
 	}
 
+
 	public void setPrimary_member(boolean primary_member) {
 		this.primary_member = primary_member;
 	}
 
+
+	public Date[] getEvent_start_dates() {
+		return event_start_dates;
+	}
+
+
+	public void setEvent_start_dates(Date[] event_start_dates) {
+		this.event_start_dates = event_start_dates;
+	}
+
+
+	public Date[] getEvent_end_dates() {
+		return event_end_dates;
+	}
+
+
+	public void setEvent_end_dates(Date[] event_end_dates) {
+		this.event_end_dates = event_end_dates;
+	}
+	
+	
 
 }
--- src/main/java/TestClaim/Test.java
+++ src/main/java/TestClaim/Test.java
@@ -1,7 +1,10 @@
 package TestClaim;
+
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -23,119 +26,190 @@ import org.kie.api.runtime.StatelessKieSession;
 import org.kie.internal.command.CommandFactory;
 import org.kie.internal.io.ResourceFactory;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+//import java.text.SimpleDateFormat;
 
 public class Test {
-	  //Delimiters Used in the CSV file
-	 private static final String CSV_Delimiter = ":";
-	
-	 
-	 public static void main(String[] args)throws Exception{
-		
+
+	public static void main(String[] args)throws Exception{
+
 		Calendar cal = Calendar.getInstance();
-	    Date today = cal.getTime();
-	    System.out.println(today);
-	    cal.add(Calendar.YEAR, -1); // to get previous year add -1
-	    Date lastYear = cal.getTime();
-	    System.out.println(lastYear);
-		
-	  //Create List for Holding CalimInfo Objects
-		  List<ClaimInfo> InputData = new ArrayList<ClaimInfo>();
-		  
-	  BufferedReader br = null;
-	  try{
-		  //Reading the csv file
-		  br = new BufferedReader(new FileReader("ClientData.csv"));
-		  
-		  /*String[] evtnames = new String[]{"Medicare Part A","Medicare Part B","Medicare Part D"};
-		  String[] evtDates = new String[]{"10/4/2017","12/7/2017","15/03/2017","16/06/2017","22/04/2017","30/11/2017"};
-		  */
-	
-		  String line = "";
-		  //Read to skip the header
-		  br.readLine();
-		  		  
-		  //Reading from the second line
-		  while ((line =br.readLine())!=null)
-		  {
-			  String[] CliamInfoInput = line.split(CSV_Delimiter);
-			 
-			  if(CliamInfoInput.length >0)
-			  {
-				  //save the Cliam INFORMATION in ClaimInfo object
-				  ClaimInfo input = new ClaimInfo(CliamInfoInput[0],Integer.parseInt(CliamInfoInput[1]),Integer.parseInt(CliamInfoInput[2]),
-						  Integer.parseInt(CliamInfoInput[3]),CliamInfoInput[4],CliamInfoInput[5],(CliamInfoInput[6].split(",")),CliamInfoInput[7],
-						  Integer.parseInt(CliamInfoInput[8]),CliamInfoInput[9],(CliamInfoInput[10].split(",")),today,lastYear,Boolean.parseBoolean(CliamInfoInput[11]));
-				  InputData.add(input);
-			  }
-		  }
-	  }catch(Exception ee){
-		  ee.printStackTrace();
-	  }finally{
-		  try{
-			  br.close();
-		  }catch(IOException ie){
-			  System.out.println("Error occured while closing the BufferedReader");
-			  ie.printStackTrace();
-		  }
-	  }
-	  
-    
-	    /*Logger LOGGER = LoggerFactory.getLogger(Test.class);
-	    try{*/
-		/*List<ClaimInfo> inputData = Arrays.asList(
-				new ClaimInfo("12345",35468,01,01,"58103","Part A" ,new String[]{"Medicare Part A", "Medicare Part B"},"Individual" ,
-						00,"3/05/2017",new String[]{"10/04/2017", "12/07/2017", "15/03/2017", "16/06/2017"},today,lastYear,true),
-				new ClaimInfo("145615",35468,01,01,"58103","Part B" ,new String[]{"Medicare Part A", "Medicare Part B","Stopped Working","Aged"},"Group" ,
-						00,"3/06/2017",new String[]{"10/01/2017","19/01/2018","10/04/2017", "12/07/2017", "15/03/2017", "16/06/2017","15/05/2016","25/01/2018"},today,lastYear,true),
-				new ClaimInfo("145616",35468,01,01,"58103","Part B" ,new String[]{"Medicare Part D", "Medicare Part B","Employeed"},"Group" ,
-						00,"13/05/2017",new String[]{"10/01/2017","19/01/2018","10/04/2017", "12/07/2017", "15/03/2017", "16/06/2017"},today,lastYear,true),
-				new ClaimInfo("145617",35468,01,01,"58103","Part B" ,new String[]{"Aged", "Medicare Part B","Employeed"},"Group" ,
-						00,"23/06/2017",new String[]{"10/01/2017","19/01/2018","10/04/2017", "12/07/2017", "15/03/2017", "16/06/2017"},today,lastYear,true)
+		Date today = cal.getTime();
+		System.out.println(today);
+		cal.add(Calendar.YEAR, -1); // to get previous year add -1
+		Date lastYear = cal.getTime();
+		System.out.println(lastYear);
+		try{
+
+
+			//reading Data from SQL Server
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			System.out.println("JDBC Driver is Loaded");
+
+			String server = "SEVEN\\SQLEXPRESS01";
+
+			String dbName = "health_care_COB_stg";
+			String jdbcUrl = "jdbc:sqlserver://"+server+";databaseName="+dbName+";integratedSecurity=true";
+			Connection sql_Connnection = DriverManager.getConnection(jdbcUrl);
+			System.out.println("Connection Established");
+			Statement sql_Statement = sql_Connnection.createStatement();
+			String query = "SELECT [EVENT_NAMES],[EVENT_START_DATES],[EVENT_END_DATES],[mbs_type],[mbs_id],[line_date]"
+					+" ,[line_num],[primacy],[proc_medicare_covered],[pvd_zipcode],[part_type],[primary_member]"
+					+",[emp_size_aged],[emp_size_disabled],[clm_id],[emp_tin]"
+					+" FROM [health_care_COB_stg].[dbo].[VW_RECOVERABLE_UPDATED]"
+					+" WHERE [EVENT_NAMES] IS NOT NULL";
+
+			ResultSet sql_ResultSet = sql_Statement.executeQuery(query);
+			
+			//Create Spark Instance
+
+			SparkConf conf = new SparkConf().setAppName("Phase-one Test").setMaster("local[2]").set("spark.executor.memory", "1g");
+
+			JavaSparkContext sc = new JavaSparkContext(conf);
+
+			KieBase rules = loadRules();
+
+			Broadcast<KieBase> broadcastRules = sc.broadcast(rules);
+
+			//Create List for Holding the Input CalimInfo Objects
+			
+			List<ClaimInfo> InputData = new ArrayList<ClaimInfo>();
+			
+			String claim_id;
+			Date line_Date_Tst;
+			int Line_Num ;
+			int primacy_value;
+			int procedure_covered;
+			int provider_Zipcode ;
+			String part_Type;
+			String event_Names;
+			String mbs_Type;
+			String event_Start_Dates;
+			long numApproved = 0;
+			String event_End_Dates = null;
+
+			//Map for Displaying the output from our rules.
+			Map<String,Boolean> output = new HashMap<String,Boolean>();
+
+			//get the input from SqlServer into our local variables
+
+			while (sql_ResultSet.next()){
+
+				claim_id = sql_ResultSet.getString("clm_id");
+
+				line_Date_Tst  = sql_ResultSet.getDate("line_date");
+
+				Line_Num = sql_ResultSet.getInt("line_num");
+
+				primacy_value = sql_ResultSet.getInt("primacy");
+
+				procedure_covered = sql_ResultSet.getInt("proc_medicare_covered");
+
+				provider_Zipcode = sql_ResultSet.getInt("pvd_Zipcode");
+
+				part_Type = sql_ResultSet.getString("part_type");
+
+				event_Names = sql_ResultSet.getString("EVENT_NAMES");
+
+				mbs_Type = sql_ResultSet.getString("mbs_type");
+
+				event_Start_Dates = sql_ResultSet.getString("EVENT_START_DATES");
+
+				event_End_Dates = sql_ResultSet.getString("EVENT_END_DATES");
+
+				Boolean primary_Memeber = sql_ResultSet.getBoolean("primary_member");
+
+				int employer_Size_Aged = sql_ResultSet.getInt("emp_size_aged");
+
+				int employer_Size_Disabled = sql_ResultSet.getInt("emp_size_disabled");
+
 				
-				);*/
-	   
-		
-		/*Scanner while hasNext {
-			csv.readline()
-			list.add(new ClaimInfo(sc.[1], sc.[2]))
-		}*/
-		
-		
-		/*List<EventInfo> eventinfo = new ArrayList<EventInfo>();
-		int i = 0;*/
-        
-		
-		
-		SparkConf conf = new SparkConf().setAppName("Phase-one Test").setMaster("local[2]").set("spark.executor.memory", "1g");
-		
-	    JavaSparkContext sc = new JavaSparkContext(conf);
+				//Calling the ClaimInfo Constructor with the inputs that we have obtained from SqlServer		 
+				InputData = Arrays.asList(new ClaimInfo(claim_id, Line_Num, primacy_value, procedure_covered, provider_Zipcode,
+						part_Type, event_Names.split(","), mbs_Type, employer_Size_Aged,employer_Size_Disabled, line_Date_Tst,
+						event_Start_Dates.split(","),event_End_Dates.split(","), today,lastYear,primary_Memeber));
+				
+				/* InputData.add(new ClaimInfo(claim_id, Line_Num, primacy_value, procedure_covered, provider_Zipcode,
+						 part_Type, event_Names.split(","), mbs_Type, employer_Size_Aged,employer_Size_Disabled, line_Date_Tst,
+						 event_Start_Dates.split(","),event_End_Dates.split(","), today,lastYear,primary_Memeber));*/
 
-	    KieBase rules = loadRules();
-	    Broadcast<KieBase> broadcastRules = sc.broadcast(rules);
+				//create the Java RDD with the claimInfo object that we have created 
 
-	    JavaRDD<ClaimInfo> claims = sc.parallelize(InputData);
+				JavaRDD<ClaimInfo> claims = sc.parallelize(InputData);
+				
+				//Broadcasting the RULES to all the nodes and 
+				//executing the rules on the chunks of data in those nodes
 
-	    long numApproved = claims.map( a -> applyRules(broadcastRules.value(), a) )
-	                                 .filter( a -> a.isRecoverable() )
-	                                 .count();
+				numApproved += claims.map( a -> applyRules(broadcastRules.value(), a) )
+						.filter( a -> ClaimInfo.isRecoverable() )
+						.count();
+				
+				String claim_Id = InputData.get(0).getClm_id();
+				
+				//Inserting the claims values into our output HashMap
+				output.put(claim_Id, ClaimInfo.isRecoverable());
 
-	    System.out.println("Number of Claims recoverable: " + numApproved);
-	    
-	    sc.close();
-	   /* }catch(Exception e){
-	    	LOGGER.error(e.getMessage(),e);
-	    }*/
-		
+
+				// inserting the Claim id and recoverability values into the table in sql server
+
+				String query2 = "insert into [health_care_COB_stg].[dbo].[Claims_Recovery_Info] "
+						+ "(CLAIM_ID, RECOVERABLE, recent_TimeStamp) values (?,?,?)";
+				
+				PreparedStatement stmt = sql_Connnection.prepareStatement(query2);
+				stmt.setString(1, claim_Id);
+				stmt.setBoolean(2, ClaimInfo.isRecoverable());
+				stmt.setTimestamp(3,new java.sql.Timestamp(today.getTime()));
+				stmt.executeUpdate();
+			
+			}
+
+
+			//Displaying our Output from the HashMap
+
+			for(Map.Entry<String, Boolean> entry : output.entrySet()){
+
+				if(entry.getValue() == true){
+					System.out.println("Claim Id : "+entry.getKey()+" is recoverable");
+
+				}else{
+					System.out.println("Claim Id : "+entry.getKey()+" is not recoverable");
+				}
+
+			}
+			//Displaying the total number of claims approved
+
+			System.out.println("Number of Claims recoverable: " + numApproved);
+
+			sc.close();
+		}catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			System.exit(2);
+		}
 	}
 
-	  public static KieBase loadRules() {
-		  KieServices kieServices = KieServices.Factory.get();
-		  Resource ruleFile = ResourceFactory.newFileResource("src/main/resources/main/rules.drl");
+
+	//Method for Loading the Rules
+	public static KieBase loadRules() {
+		KieServices kieServices = KieServices.Factory.get();
+		Resource ruleFile = ResourceFactory.newFileResource("src/main/resources/main/rules.drl");
 		//Resource ruleFile = ResourceFactory.newClassPathResource("/src/main/resources/main/KB-Rules.xls");
-		 
+
 		KieFileSystem kieFileSystem = kieServices.newKieFileSystem().write(ruleFile);
 		KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
 		kieBuilder.buildAll();
@@ -143,13 +217,13 @@ public class Test {
 		ReleaseId krDefaultReleaseId = kieRepository.getDefaultReleaseId();
 		KieContainer kieContainer = kieServices.newKieContainer(krDefaultReleaseId);
 		return kieContainer.getKieBase();
-	  }
-
-	  public static ClaimInfo applyRules(KieBase base, ClaimInfo a) {
-	    StatelessKieSession session = base.newStatelessKieSession();
-	    session.execute(CommandFactory.newInsert(a));
-	    return a;
-	  }
 	}
+	// Method for applying the rules to the Input Data
+	public static ClaimInfo applyRules(KieBase base, ClaimInfo a) {
+		StatelessKieSession session = base.newStatelessKieSession();
+		session.execute(CommandFactory.newInsert(a));
+		return a;
+	}
+}
 
 
